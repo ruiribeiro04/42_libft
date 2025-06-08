9e6fce21c6d7c3c750d97d4cee7ba5b58b60e286
Rui Ribeiro <contacto@ruiribeiro.me>
2025-06-08 22:54:08 +0100

    removed duplicated functions

--- 42_get_next_line/get_next_line.h
+++ 42_get_next_line/get_next_line.h
@@ -21,10 +21,5 @@
 # include <unistd.h>
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char *s1, char *s2);
-char	*ft_strchr(char *s, int c);
-size_t	ft_strlen(char *s);
-char	*ft_strdup(char *s1);
-char	*ft_substr(char *s, unsigned int start, size_t len);
 
 #endif
